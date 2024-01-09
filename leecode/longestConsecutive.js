0d9ecb2d37aa35c7062994f11c3df8f0ef0f88eb
lilinji-TT <112566451+lilinji-TT@users.noreply.github.com>
2024-01-09 16:27:36 +0800

    Update longestConsecutive.js

--- leecode/longestConsecutive.js
+++ leecode/longestConsecutive.js
@@ -1,6 +1,7 @@
+// 不符合题目的 O(n)要求
 var longestConsecutive = function (nums) {
   nums.sort((a, b) => a - b);
-  
+
   const map = new Map();
 
   // 这一步是核心，主要是为了处理
@@ -12,8 +13,8 @@ var longestConsecutive = function (nums) {
 
   let ans = 0;
 
-  for (const [key, vlaue] of map) {
-    ans = Math.max(ans,vlaue);
+  for (const [, vlaue] of map) {
+    ans = Math.max(ans, vlaue);
   }
 
   return ans;
@@ -21,3 +22,39 @@ var longestConsecutive = function (nums) {
 
 console.log(longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1])); // 9
 console.log(longestConsecutive([0, 3, 7, 2, 5, 8, 4])); // 4
+
+// O(n)
+var _longestConsecutive = function (nums) {
+  const map = new Map();
+
+  let ans = 0;
+
+  for (const num of nums) {
+    // 避免重复
+    if (!map.has(num)) {
+      let preLen = map.get(num - 1) || 0; // 获取当前项的前一项对应的序列长度
+      let nextLen = map.get(num + 1) || 0; // 获取当前项的后一项对应的序列长度
+      let curLen = preLen + 1 + nextLen; // 计算当前项
+      map.set(num, curLen); // 将当前项对应的序列长度存入
+      ans = Math.max(ans, curLen); // 更新结果
+      map.set(num + nextLen, curLen); // 更新右端
+      map.set(num - preLen, curLen); // 更新左端
+    }
+
+    console.log(map);
+  }
+
+  return ans;
+};
+
+/**
+ * Map(1) { 100 => 1 }
+ * Map(2) { 100 => 1, 1 => 1 }
+ * Map(3) { 100 => 1, 1 => 1, 200 => 1 }
+ * Map(4) { 100 => 1, 1 => 2, 200 => 1, 2 => 2 }
+ * Map(5) { 100 => 1, 1 => 2, 200 => 1, 2 => 2, 4 => 1 }
+ * Map(6) { 100 => 1, 1 => 4, 200 => 1, 2 => 2, 4 => 4, 3 => 4 }
+ */
+_longestConsecutive([100, 1, 200, 2, 4, 3]);
+console.log(_longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1])); // 9
+console.log(_longestConsecutive([100, 1, 200, 2, 4, 3])); // 4
