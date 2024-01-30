d9b9a88148c3c6739138bbfad0aacbb06ddf9ba2
lilinji-TT <112566451+lilinji-TT@users.noreply.github.com>
2024-01-30 17:17:49 +0800

    Update minWindow.js

--- leecode/minWindow.js
+++ leecode/minWindow.js
@@ -5,13 +5,44 @@
  */
 var minWindow = function (s, t) {
   let result = "";
-  if (s.length < t.length) return result;
-
+  let minLen = Infinity;
   const map = new Map();
 
   for (const s of t) {
     map.set(s, (map.get(s) || 0) + 1);
   }
 
-  for (let i = 0; i < s.length; i++) {}
+  let charCount = map.size;
+
+  for (let i = 0, j = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      let l = map.get(s[i]);
+      map.set(s[i], l - 1);
+      if (l === 0) {
+        charCount--;
+      }
+    }
+
+    while (charCount === 0) {
+      if (i - j < minLen) {
+        minLen = i - j;
+        result = s.slice(j, i);
+      }
+
+      if (map.has(s[j])) {
+        let l = map.get(s[j]);
+        map.set(s[j], l + 1);
+
+        if (l > 0) {
+          charCount++;
+        }
+      }
+
+      j++;
+    }
+  }
+  
+  return result;
 };
+
+console.log(minWindow("window", "ow"));
